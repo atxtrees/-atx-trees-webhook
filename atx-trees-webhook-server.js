49822b6582b60261249d83f023c210b6bcd5bf6d
atxtrees <albert@atxtrees.com>
2026-08-21 08:42:42 -0500

    Fix CORS to include DELETE method for bookings

--- atx-trees-webhook-server.js
+++ atx-trees-webhook-server.js
@@ -141,7 +141,7 @@ async function loadAllData() {
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
   if (req.method === "OPTIONS") return res.sendStatus(200);
   next();
 });
